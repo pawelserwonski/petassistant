2cfa38e6bbc140edb0a288de6b3d7c3f78af99dc
Paweł Serwoński <pawel.serwonski@gmail.com>
2018-11-24 18:01:11 +0100

    first commit

---
+++ src/main/java/ski/serwon/petassistant/PetassistantApplication.java
@@ -0,0 +1,12 @@
+package ski.serwon.petassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetassistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetassistantApplication.class, args);
+    }
+}
---
+++ src/main/java/ski/serwon/petassistant/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ski.serwon.petassistant;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PetassistantApplication.class);
+    }
+
+}
