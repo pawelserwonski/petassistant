992fce1fee7da1be5fa34edd32d6a568ff1af0d4
pserwonski <pawel.serwonski@gmail.com>
2019-03-17 21:04:20 +0100

    added tests for AnimalController

--- src/test/java/ski/serwon/petassistant/unit/controller/AnimalControllerTest.java
+++ src/test/java/ski/serwon/petassistant/unit/controller/AnimalControllerTest.java
@@ -1,23 +1,24 @@
 package ski.serwon.petassistant.unit.controller;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.TestInstance;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import ski.serwon.petassistant.PetassistantApplication;
 import ski.serwon.petassistant.controller.animal.AnimalController;
-import ski.serwon.petassistant.dao.animal.AnimalDao;
-import ski.serwon.petassistant.dao.user.UserDao;
+import ski.serwon.petassistant.dto.animal.AnimalDTO;
 import ski.serwon.petassistant.entity.animal.Animal;
 import ski.serwon.petassistant.entity.user.User;
 import ski.serwon.petassistant.mapper.animal.AnimalMapper;
@@ -26,11 +27,12 @@ import ski.serwon.petassistant.mockinitializer.LoginDetailsServiceMockInitialize
 import ski.serwon.petassistant.security.LoginDetailsService;
 import ski.serwon.petassistant.service.animal.AnimalService;
 
+import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = PetassistantApplication.class)
@@ -38,8 +40,8 @@ import static org.mockito.ArgumentMatchers.anyLong;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AnimalControllerTest {
     private MediaType contentType = MediaType.APPLICATION_JSON_UTF8;
-    private MockMvc mockMvc;
     private User mockedUser;
+    private Animal mockedAnimal;
     private List<Animal> animalMockList;
 
     @Autowired
@@ -54,22 +56,123 @@ public class AnimalControllerTest {
 
     private AnimalController animalController;
 
-    @BeforeAll
+    @BeforeEach
     public void setUpMocks() {
         MockitoAnnotations.initMocks(this);
         animalController = new AnimalController(animalService, animalMapper, loginDetailsService);
+        prepareMockedUserAndAnimals();
         AnimalServiceMockInitializer.setUpAnimalServiceMock(animalService, animalMockList);
-        LoginDetailsServiceMockInitializer.setUpLoginDetailsServiceMock();
+        LoginDetailsServiceMockInitializer.setUpLoginDetailsServiceMock(loginDetailsService, mockedUser);
     }
 
-    @BeforeEach
-    public void prepareMockedUserAndAnimals() {
+    private void prepareMockedUserAndAnimals() {
         animalMockList = new LinkedList<>();
-        mockedUser = new User(1L, "Lorem", "Ipsum",
-                "foo@bar.com", "pass", null, animalMockList);
+        mockedUser = User.builder().id(1L)
+                .firstName("Lorem")
+                .lastName("Ipsum")
+                .email("foo@bar.com")
+                .password("pass")
+                .animals(animalMockList)
+                .build();
+        mockedAnimal = Animal.builder()
+                .id(0L)
+                .owner(mockedUser)
+                .species("Foo")
+                .breed("Bar")
+                .birthDate(LocalDate.now())
+                .name("Lorem")
+                .vaccines(new LinkedList<>())
+                .walks(new LinkedList<>())
+                .feeds(new LinkedList<>())
+                .diseasesHistory(new LinkedList<>())
+                .vetVisitsHistory(new LinkedList<>())
+                .build();
+        animalMockList.add(mockedAnimal);
     }
 
+    @Test
+    public void shouldNotFound_getAnimalWithNonExistingId() throws Exception{
+        long id = 1500_100_900;
+        ResponseEntity entity = animalController.getAnimalWithId(id);
+        Assertions.assertEquals(ResponseEntity.notFound().build(), entity);
+    }
 
+    @Test
+    public void shouldFind_getAnimalWithExistingId() throws Exception{
+        long id = 0;
+        ResponseEntity entity = animalController.getAnimalWithId(id);
+        AnimalDTO mappedAnimal = animalMapper.mapModelToDTO(mockedAnimal, AnimalDTO.builder().build());
+        Assertions.assertEquals(ResponseEntity.ok(mappedAnimal), entity);
+    }
 
+    @Test
+    public void shouldNofFound_getAnimalWithNegativeId() throws Exception{
+        long id = -1;
+        ResponseEntity entity = animalController.getAnimalWithId(id);
+        Assertions.assertEquals(ResponseEntity.notFound().build(), entity);
+    }
 
+    @Test
+    public void shouldAddToListAndReturnAdded_addAnimal() throws Exception {
+        AnimalDTO addedAnimalDTO = AnimalDTO.builder()
+                .species("FOO")
+                .breed("BAR")
+                .birthDate(LocalDate.now())
+                .name("LOREM")
+                .build();
+        ResponseEntity entity = animalController.createAnimal(addedAnimalDTO);
+        Animal addedAnimal = (Animal) entity.getBody();
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(ResponseEntity.ok(addedAnimal), entity),
+                () -> MatcherAssert.assertThat(animalMockList, Matchers.hasItem(addedAnimal))
+        );
+    }
+
+    @Test
+    public void shouldAddToListWithNewIdAndReturnAdded_addAnimalWithoutPredefinedId() throws Exception {
+        AnimalDTO addedAnimalDTO = AnimalDTO.builder()
+                .species("FOO")
+                .breed("BAR")
+                .birthDate(LocalDate.now())
+                .name("LOREM")
+                .id(10000L)
+                .build();
+        ResponseEntity entity = animalController.createAnimal(addedAnimalDTO);
+        Animal addedAnimal = (Animal) entity.getBody();
+        Assertions.assertNotEquals(addedAnimalDTO.getId(), addedAnimal.getId());
+    }
+
+    @Test
+    public void shouldUpdateInListAndReturnUpdated_updateAnimalWithSpecifiedId() throws Exception {
+        AnimalDTO updatedAnimalDTO = AnimalDTO.builder()
+                .species("FOO")
+                .breed("BAR")
+                .birthDate(LocalDate.now())
+                .name("LOREM")
+                .build();
+        ResponseEntity entity = animalController.updateAnimal(updatedAnimalDTO, 0L);
+        Animal updatedAnimal = (Animal) entity.getBody();
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(ResponseEntity.ok(updatedAnimal), entity),
+                () -> Assertions.assertEquals(updatedAnimal, animalMockList.get(0))
+        );
+    }
+
+    @Test
+    public void shouldNotUpdateInListAndReturnNull_updateAnimalWithIdNotInList() throws Exception {
+        AnimalDTO updatedAnimalDTO = AnimalDTO.builder()
+                .species("FOO")
+                .breed("BAR")
+                .birthDate(LocalDate.now())
+                .name("LOREM")
+                .build();
+        Animal[] beforeUpdate = animalMockList.toArray(new Animal[0]);
+        ResponseEntity entity = animalController.updateAnimal(updatedAnimalDTO, 10000L);
+        Animal[] afterUpdate = animalMockList.toArray(new Animal[0]);
+        Animal updatedAnimal = (Animal) entity.getBody();
+        Assertions.assertAll(
+                () -> Assertions.assertNull(updatedAnimal),
+                () -> Assertions.assertArrayEquals(beforeUpdate, afterUpdate)
+        );
+    }
 }
